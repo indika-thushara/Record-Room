c82271586a515b6ec2b8039cce0f9809a139a30a
indika-thushara <indika120@gmail.com>
2025-09-29 23:06:58 +0530

    Dashboard total file count function done

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
 </head>
 
 <body>
-    <?php include "header.php" ?>
+    <?php
+    include "header.php";
+    require_once "db_connection.php";
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
@@ -46,42 +49,50 @@
                 </div>
             </div>
 
-        <div class="row justify-content-center mt-4">
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Number of files in system.</p>
+
+            <div class="row justify-content-center mt-4">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <?php
+                            $sql = "call filescount()";
+                            $result = mysqli_query($conn, $sql);
+                            if ($result)
+                                $row = mysqli_fetch_assoc($result);                           
+                            ?>
+                            <?php echo "<h3>". $row['filescount'],"</h3>" ?>
+                            <p class="h5">Files are in the system.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Files are get back to the office.</p>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Files are get back to the office.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Number of files to be distroied.</p>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Number of files to be distroied.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        document.getElementById('recback').addEventListener('change', function() {
-            if (this.checked) {
-                document.getElementById('bakrron').value = '';
-            }
-        });
-    </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script>
+            document.getElementById('recback').addEventListener('change', function() {
+                if (this.checked) {
+                    document.getElementById('bakrron').value = '';
+                }
+            });
+        </script>
 </body>
 
 </html>
